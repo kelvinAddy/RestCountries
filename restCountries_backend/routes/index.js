eb9dcb84b098649fcff2b0b18fe40f4495fd9151
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-11 02:13:19 +0000

    Added server error responses for failed API requests on the backend

--- restCountries_backend/routes/index.js
+++ restCountries_backend/routes/index.js
@@ -1,37 +1,41 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const { CountryModel } = require("../models/Countries.js");
-const path = require("path");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const { CountryModel } = require('../models/Countries.js');
+const path = require('path');
 
 app.use(express.json());
 
-app.use(express.static("./public/dist"));
+app.use(express.static('./public/dist'));
 
-morgan.token("body", (req, res) => {
+morgan.token('body', (req, res) => {
   JSON.stringify(req.body);
 });
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+);
 
-app.get("/countries/data", (req, res, next) => {
+app.get('/countries/data', (req, res, next) => {
   CountryModel.find({})
     .then((data) => res.json(data))
     .catch(next);
 });
 
-app.get(/^\/countries\/[^/]+$/, (req, res) => res.sendFile(path.resolve("./public/dist/index.html")));
+app.get(/.*/, (req, res) =>
+  res.sendFile(path.resolve('./public/dist/index.html')),
+);
 
 app.use((error, req, res, next) => {
-  console.log(error.message);
+  res.status(500).json({ error: error.message });
 });
 
 mongoose
   .connect(process.env.MONGODB_URI, { family: 4 })
   .then(() => {
-    console.log("Database connected");
+    console.log('Database connected');
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on PORT: ${process.env.PORT}`);
     });
